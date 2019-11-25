47d087bc916a34e0b53ba9c547579b1833ee20a7
zhangjipeng <2652512560@qq.com>
2019-11-25 10:03:59 +0800

    项目初始化

---
+++ pages/hot/hot.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		热门
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
